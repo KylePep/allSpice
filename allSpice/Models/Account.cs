d9dd42c2aa864ec655e11745095014cc1b3c5dc3
Kyle <kylepeppersack@hotmail.com>
2023-08-16 15:18:30 -0600

    Init recipe Table

--- allSpice/Models/Account.cs
+++ allSpice/Models/Account.cs
@@ -3,7 +3,19 @@ namespace allSpice.Models;
 public class Account
 {
   public string Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
   public string Name { get; set; }
   public string Email { get; set; }
   public string Picture { get; set; }
+}
+
+public class Profile
+{
+  public string Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
+  public string Name { get; set; }
+  public string Picture { get; set; }
+
 }
--- allSpice/dbSetup.sql
+++ allSpice/dbSetup.sql
@@ -6,3 +6,17 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+
+CREATE TABLE recipe(
+  id INT NOT NULL PRIMARY KEY,
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  title VARCHAR(255) NOT NULL,
+  instructions VARCHAR(1000) NOT NULL,
+  img VARCHAR(700) NOT NULL,
+  category ENUM ('Cheese','Italian','Soup','Mexican','Specialty Coffee'),
+  creatorId VARCHAR(255) NOT NULL,
+  FOREIGN KEY(creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+
+DROP TABLE recipe
